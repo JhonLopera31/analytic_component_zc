91e453952aeb2905d62d92a0d210b69f9459c404
jhon.lopera <anderson.lopera@sourcemeridian.com>
2022-11-05 19:00:05 -0500

    feat: starting trainer class

--- analytics/modules/forecasting/trainer.py
+++ analytics/modules/forecasting/trainer.py
@@ -1,5 +1,7 @@
 from pandas import DataFrame
 from modules.logs.loggers import GeneralLogger
+from numpy import array
+
 
 class ForecastingTrainer:
 
@@ -7,3 +9,26 @@ class ForecastingTrainer:
     def DummyTraining(cls, data: DataFrame) -> DataFrame:
         GeneralLogger.put_log("Dummy training")
         return data
+    
+        
+    @classmethod
+    def preprocessing(data: DataFrame):
+        pass
+    
+    @classmethod
+    def setup_neuronal_network(cls):
+        pass
+    
+    @classmethod
+    def fit_model(cls):
+        pass
+    
+    
+    @staticmethod
+    def convert_to_matrix(data: array, look_back: int):
+        X, Y = [], []
+        for i in range(len(data)-look_back):
+            d=i+look_back  
+            X.append(data[i:d,])
+            Y.append(data[d,])
+            return array(X), array(Y)
